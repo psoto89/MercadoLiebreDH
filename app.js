05c72c99861a83cd42674746fa47bebdcf87f3d4
psoto89 <psotofranco@gmail.com>
2021-10-11 22:13:10 -0500

    Avance de Formularios

--- app.js
+++ app.js
@@ -10,5 +10,13 @@ app.get('/', (req, res) => {
     res.sendFile( path.join (__dirname,  './views/home.html'));
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile( path.join (__dirname,  './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile( path.join (__dirname,  './views/login.html'));
+});
+
 
 app.listen(PUERTO, () => console.log("Servidor esta corriendo sin problemas"));
